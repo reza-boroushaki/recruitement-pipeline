04fedca37888ce9befb3b945e9be6fb13704078a
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 17:58:05 +0400

    added button to generate new user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useHistory} from 'react-router-dom';
+import { getAllUser, createUser, deleteUser } from '../api';
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  let history = useHistory();
+  const startForm = e => {
+    e.preventDefault();
+    const unique = new Date().getTime();
+    createUser(unique).then(res => {
+        if(res.stat){
+          history.push(`/edit/${unique}`, {state: {id: unique}});
+        }
+    })
+    .catch(() => console.log("error"));
+}
   return (
-    <div>Home</div>
+    <div>
+      <button onClick={startForm}>Start new candidate</button>
+    </div>
   )
 }
