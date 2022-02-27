3dc71f0c7ca11efabbbcd368adf483a0aa7c0b87
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 18:40:12 +0400

    added header component

---
+++ src/components/Form/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.scss';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Candidate Details</h1>
+        </header>
+    )
+}
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Form from '../components/Form';
+import Header from '../components/Form/Header';
 
 export default function Edit(props) {
   return (
     <div className='container'>
+      <Header />
       <Form userID={props.match.params.id}/>
     </div>
   )
