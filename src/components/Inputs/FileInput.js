f02ddee787a5d78cadc1244793f97fda95eb00e6
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 22:04:27 +0400

    updated file input

--- src/components/Inputs/FileInput.js
+++ src/components/Inputs/FileInput.js
@@ -1,12 +1,13 @@
 import { useState, useRef, memo } from 'react'
-import { acceptFileTypes } from '../_helpers'
+import { acceptFileTypes } from '../_helpers';
+import './fileInput.scss';
 
 function FileInput({ updateForm, inputState }) {
     const [selectedFile, setSelectedFile] = useState(inputState);
+    const [loaded, setLoaded] = useState(inputState ? true : false);
     const droppedFileRef = useRef(null);
     const dragOverHandler = e => {
         e.preventDefault();
-        console.log('File(s) in drop zone');
     }
     const dropHandler = e => {
         e.preventDefault();
@@ -17,28 +18,42 @@ function FileInput({ updateForm, inputState }) {
         }
         let reader = new FileReader();
         reader.onload = function(){
-            updateForm('resume', {file: reader.result, name: files[0].name});
-            setSelectedFile({file: reader.result, name: files[0].name});
+            updateForm('resume', {file: reader.result, name: files[0].name, type: files[0].type});
+            setSelectedFile({file: reader.result, name: files[0].name, type: files[0].type});
         }
         reader.onloadend = function(){
             console.log("load ended")
+            setLoaded(true);
         }
         if (files.length) {
             reader.readAsDataURL(files[0])
         }
     }
     return (
-        <>
+        <div className='fileWrapper'>
+            <p className='label'><label>CV</label></p>
             <div className="drop_zone" onDrop={dropHandler} onDragOver={dragOverHandler}>
-                <p>Drag file to this Drop Zone</p>
+                <div className='dropText'>
+                    <b>Drag files here to upload</b>
+                    <p>or</p>
+                    <span>CHOOSE FILE TO UPLOAD</span>
+                </div>
                 <input type="file" name="resume" accept={acceptFileTypes.map(item => `${item.type},`)} title='' onChange={dropHandler}/>
             </div>
             <i>Required</i>
-            <p ref={droppedFileRef}>{selectedFile?.name}</p>
             {
-                selectedFile?.file && <a href={selectedFile?.file} download>download</a>
+                loaded
+                ?
+                <div className='fileName'>
+                    <p ref={droppedFileRef}>{selectedFile?.name}</p><span>DONE</span>
+                </div>
+                :
+                ''
             }
-        </>
+            {
+                selectedFile?.file && <a className='downloadLink' href={selectedFile?.file} download>Download</a>
+            }
+        </div>
     )
 }
 
