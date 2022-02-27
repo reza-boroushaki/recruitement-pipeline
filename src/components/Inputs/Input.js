b64e20cbf39f4690c17f35bea1a938541b29ae82
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 17:13:52 +0400

    added edit method for inputs component

--- src/components/Inputs/Input.js
+++ src/components/Inputs/Input.js
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Input({ attr }) {
     const { type, title, name, required = false } = attr;
     const [inputValue, setInputValue] = useState('');
     const [submitValue, setSubmitValue] = useState(inputState ? true : false);
 
+    const inputRef = useRef(null);
+
     const handleInput = e => {
         setInputValue(e.target.value);
+    }
+
+    const handleEdit = e => {
+        e.preventDefault();
+        setSubmitValue(false);
+        inputRef?.current.focus();
     }
 
     return (
         <div>
             <p><label>{title ? title : ''}</label></p>
-            <input type={type} name={name} value={inputValue} onChange={handleInput} title=''/>
+            <input ref={inputRef} type={type} name={name} value={inputValue} onChange={handleInput} onFocus={handleEdit} title=''/>
         </div>
     )
 }
