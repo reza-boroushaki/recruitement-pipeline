3bd497bee88294a1e6f5c0e4eaee00de1ff9071b
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 17:05:44 +0400

    added stage changing dropdown + stages helper

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useState, useEffect, useCallback } from 'react';
+import { stages } from '../_helpers';
 
 export default function Form() {
-  return (
-    <div>Form</div>
-  )
+    const [stage, setStage] = useState(0);
+    const stageChange = useCallback(val => {
+        setStage(val);
+    }, [])
+    return (
+        <>
+            <form>
+                <div className="leftCol">
+                    <div className="stageWrapper">
+                        <select style={{backgroundColor: `${stages[stage].color}`}} onChange={e => stageChange(e.target.value)} value={stage}>
+                            {
+                                stages.map((item, index) => <option key={index} value={index}>{item.name}</option>)
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="rightCol">
+
+                </div>
+            </form>
+        </>
+    )
 }
---
+++ src/components/_helpers/index.js
@@ -0,0 +1,8 @@
+export const stages = [
+    {name: "First contact", color: '#10EF7C'},
+    {name: "Interview", color: '#1013EF'},
+    {name: "Tech", color: 'navy'},
+    {name: "Assignment", color: '#EFEC10'},
+    {name: "Rejected", color: '#FA1B1B'},
+    {name: "Hired", color: 'orange'}
+]
