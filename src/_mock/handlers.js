e08c9af459facdcc23056bc387d82d091b0b1acb
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 18:03:52 +0400

    added fetch one user + delete + update in handler

--- src/_mock/handlers.js
+++ src/_mock/handlers.js
@@ -9,6 +9,16 @@ const handlers = [
     );
   }),
 
+  rest.get("/getUser/:id", (req, res, context) => {
+    const { id } = req.params;
+    const candidate = localStorage.getItem("candidates");
+    const user = JSON.parse(candidate).find(item => item.id === id);
+    return res(
+    //   context.status(400),
+      context.json(user)
+    );
+  }),
+
   rest.post("/newUser", (req, res, context) => {
       const user = localStorage.getItem("candidates");
       const obj = {
@@ -43,6 +53,31 @@ const handlers = [
         context.json({ stat: 1 })
       );
   }),
+
+  rest.patch("/updateUser/:id", (req, res, context) => {
+    const { id } = req.params;
+    const user = localStorage.getItem("candidates");
+    const body = JSON.parse(req.body);
+    let parsed = JSON.parse(user);
+    const index = parsed.findIndex(item => item.id === id);
+    parsed[index] = body;
+    localStorage.setItem("candidates", JSON.stringify(parsed));
+    return res(
+      context.status(200),
+      context.json({ stat: 1 })
+    );
+  }),
+  
+  rest.delete("/deleteUser/:id", (req, res, context) => {
+    const { id } = req.params;
+    const user = localStorage.getItem("candidates");
+    let parsed = JSON.parse(user).filter(item => item.id !== id);
+    localStorage.setItem("candidates", JSON.stringify(parsed));
+    return res(
+      context.status(200),
+      context.json(parsed)
+    );
+  })
 ];
 
 export { handlers, rest };
