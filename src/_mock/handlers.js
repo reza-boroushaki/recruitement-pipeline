9cf9edb4a092ff1729c14398c0380bf05ee73caa
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 18:02:45 +0400

    added get all user method in handler

--- src/_mock/handlers.js
+++ src/_mock/handlers.js
@@ -1,40 +1,48 @@
 import { rest } from "msw";
 
 const handlers = [
-    rest.post("/newUser", (req, res, context) => {
-        const user = localStorage.getItem("candidates");
-        const obj = {
-          'id': req.body, 
-          'full_name': '',
-          'email': '',
-          'phone_number': '',
-          'min_salary': '',
-          'max_salary': '',
-          'skills': [],
-          'seniority': '',
-          'experience': '',
-          'resume': [],
-          'actions': [{
-            type: 'added',
-            title: '',
-            from: '',
-            to: '',
-            time: new Date().getTime()
-        }]
-        }
-        if(!!user){
-          const parsed = JSON.parse(user);
-          parsed.push(obj);
-          localStorage.setItem("candidates", JSON.stringify(parsed));
-        }else{
-          localStorage.setItem("candidates", JSON.stringify([obj]));
-        }
-    
-        return res(
-          context.status(200),
-          context.json({ stat: 1 })
-        );
-    }),
+  rest.get("/getAllUser", (req, res, context) => {
+    const user = localStorage.getItem("candidates");
+    return res(
+      context.status(200),
+      context.json(JSON.parse(user))
+    );
+  }),
+
+  rest.post("/newUser", (req, res, context) => {
+      const user = localStorage.getItem("candidates");
+      const obj = {
+        'id': req.body, 
+        'full_name': '',
+        'email': '',
+        'phone_number': '',
+        'min_salary': '',
+        'max_salary': '',
+        'skills': [],
+        'seniority': '',
+        'experience': '',
+        'resume': [],
+        'actions': [{
+          type: 'added',
+          title: '',
+          from: '',
+          to: '',
+          time: new Date().getTime()
+      }]
+      }
+      if(!!user){
+        const parsed = JSON.parse(user);
+        parsed.push(obj);
+        localStorage.setItem("candidates", JSON.stringify(parsed));
+      }else{
+        localStorage.setItem("candidates", JSON.stringify([obj]));
+      }
+  
+      return res(
+        context.status(200),
+        context.json({ stat: 1 })
+      );
+  }),
 ];
 
 export { handlers, rest };
