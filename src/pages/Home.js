46c5c43af7508e835ce20fb4a5a0d1073e0f5894
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-28 01:16:52 +0400

    styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { getAllUser, createUser, deleteUser } from '../api';
+import { stages } from '../components/_helpers';
+import './home.scss';
 
 export default function Home() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
   let history = useHistory();
   const startForm = e => {
     e.preventDefault();
@@ -24,33 +27,40 @@ export default function Home() {
     })
     .catch((e) => console.log(e));
   }
+  const editUser = (e, id) => {
+    e.preventDefault();
+    history.push(`/edit/${id}`, {state: {id}});
+  }
   useEffect(() => {
     getAllUser().then(user => {
         if(!!user){
             setData(user);
+            setLoading(false);
         }
     })
     .catch((e) => console.log(e));
   }, []);
+  if(loading){
+    return <p className='loading'>Loading...</p>
+  }
   return (
-    <div>
+    <div className='container homeWrapper'>
       {
         data?.length
         ?
         data?.map((item, index) => (
-            <div key={index}>
-                {item.id}
-                <Link to={{
-                    pathname: `/edit/${item.id}`,
-                    state: {id: item.id}
-                }}>Edit</Link>
-                <button onClick={e => removeUser(e, item.id)}>delete</button>
+            <div className='users' key={index}>
+                <p>{item.full_name}</p>
+                <div className='buttonWrappers'>
+                  <button className='edit' onClick={e => editUser(e, item.id)}>Edit</button>
+                  <button className='delete' onClick={e => removeUser(e, item.id)}>delete</button>
+                </div>
             </div>
         ))
         :
         <p>No candidates found</p>
       }
-      <button onClick={startForm}>Start new candidate</button>
+      <p className='startCandidateButton'><button type='button' onClick={startForm}>Start new candidate</button></p>
     </div>
   )
 }
