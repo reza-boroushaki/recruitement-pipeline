25bc5560ca713238d369f1035b484ce5532e7f55
reza-boroushaki <75382528+reza-boroushaki@users.noreply.github.com>
2022-02-27 17:34:48 +0400

    added drop and drag over methods

--- src/components/Inputs/FileInput.js
+++ src/components/Inputs/FileInput.js
@@ -1,7 +1,15 @@
 import { useState, useRef } from 'react'
 
 export default function FileInput({ updateForm, inputState }) {
+    const [selectedFile, setSelectedFile] = useState(inputState);
+    const droppedFileRef = useRef(null);
     return (
-        <div>FileInput</div>
+        <>
+            <div className="drop_zone" onDrop={dropHandler} onDragOver={dragOverHandler}>
+                <p>Drag file to this Drop Zone</p>
+                <input type="file" name="resume" accept={acceptFileTypes.map(item => `${item.type},`)} title='' onChange={dropHandler}/>
+            </div>
+            <i>Required</i>
+        </>
     )
 }
